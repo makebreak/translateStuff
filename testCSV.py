6019beebfbd63eff4f723c8b958bcd938f6a21df
makebreak <andystudent0@gmail.com>
2019-09-13 00:26:10 -0400

    added source language

--- testCSV.py
+++ testCSV.py
@@ -9,7 +9,10 @@ in_file = open(ORIGINAL_FILE, "r", encoding='utf-8')
 reader = csv.reader(in_file)
 TRANSLATED_FILE = str(config.THIS_DIR / "out.csv")
 out_file = open(TRANSLATED_FILE, "w", encoding='utf-8')
+#fieldnames = ['title','summary', 'translatedText', 'input']
 writer = csv.writer(out_file)
+#writer = csv.DictWriter(out_file, fieldnames=fieldnames)
+#writer.writeheader()
 
 # Google credentials
 os.environ["GOOGLE_APPLICATION_CREDENTIALS"] = file.dirname
@@ -22,26 +25,24 @@ translate_client = translate.Client()
 
 #text = u'Hello, world! Where did my nose go? How inconvenient.'
 # The target language
-target = 'ru'
+source= 'ru'
+target = 'en'
 
 # for each row in the csv file being read
 # translate the third cell
 # The text to translate
 for row in reader:
-    print(row[3])
-    text = row[3]
+#    for column in row:
+    print(row[0])
+    text = row[0]
     # translate text
     translation = translate_client.translate(
         text,
+        source_language=source,
         target_language=target)
-    row[3] = translation
+    row[0] = translation['translatedText']
     print(u'Translation: {}'.format(translation['translatedText']))
     writer.writerow(row)
-
-# translate text
-#translation = translate_client.translate(
-#    text,
-#    target_language=target)
 
 #print(u'Text: {}'.format(text))
 #print(u'Translation: {}'.format(translation['translatedText']))
