c4ea6b3da24a183f9d74aa187eb78a8ec418e98c
makebreak <andystudent0@gmail.com>
2019-09-13 00:37:04 -0400

    deleted unused code

--- testCSV.py
+++ testCSV.py
@@ -9,10 +9,7 @@ in_file = open(ORIGINAL_FILE, "r", encoding='utf-8')
 reader = csv.reader(in_file)
 TRANSLATED_FILE = str(config.THIS_DIR / "out.csv")
 out_file = open(TRANSLATED_FILE, "w", encoding='utf-8')
-#fieldnames = ['title','summary', 'translatedText', 'input']
 writer = csv.writer(out_file)
-#writer = csv.DictWriter(out_file, fieldnames=fieldnames)
-#writer.writeheader()
 
 # Google credentials
 os.environ["GOOGLE_APPLICATION_CREDENTIALS"] = file.dirname
@@ -23,14 +20,11 @@ from google.cloud import translate
 # Instantiates a client
 translate_client = translate.Client()
 
-#text = u'Hello, world! Where did my nose go? How inconvenient.'
 # The target language
 source= 'ru'
 target = 'en'
 
-# for each row in the csv file being read
-# translate the third cell
-# The text to translate
+# for each row in the csv file being read, translate the first cell
 for row in reader:
 #    for column in row:
     print(row[0])
@@ -43,9 +37,6 @@ for row in reader:
     row[0] = translation['translatedText']
     print(u'Translation: {}'.format(translation['translatedText']))
     writer.writerow(row)
-
-#print(u'Text: {}'.format(text))
-#print(u'Translation: {}'.format(translation['translatedText']))
 
 in_file.close()
 out_file.close()
