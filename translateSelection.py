1ec056fb79f5dd6ee6b41f5ae2e5992a47c5fe88
makebreak <andystudent0@gmail.com>
2019-11-05 19:16:55 -0500

    added command line args source, country, target

--- translateSelection.py
+++ translateSelection.py
@@ -1,4 +1,4 @@
-import os, file, csv, config, dataset, time, collections
+import os, file, csv, config, dataset, time, collections, sys
 
 # Google credentials
 os.environ["GOOGLE_APPLICATION_CREDENTIALS"] = file.dirname
@@ -27,7 +27,7 @@ def translateFunc(text):
         source_language_code=source,
         parent=parent_code
     )
-    # response.translations is a Tranlation object
+    # response.translations is a Translation object
     for translation in response.translations:
         print(u'Translation: {}'.format(translation.translated_text))
         translatedText = translation.translated_text 
@@ -35,11 +35,11 @@ def translateFunc(text):
         return translatedText 
 
 # in file db
-ORIGINAL_FILE = str(config.THIS_DIR / "in.db")
+ORIGINAL_FILE = str(config.BASE_DIR / "crawled_apps.db")
 
 # db to write to
 SELECTION_FILE = str(config.THIS_DIR / "selection.db")
-TRANSLATED_FILE = str(config.THIS_DIR / "translatedSelection.db")
+TRANSLATED_FILE = str(config.THIS_DIR / "translated_apps.db")
 
 # Connect to original db with scraped data 
 dbApps = dataset.connect('sqlite:///{}'.format(ORIGINAL_FILE))
@@ -51,17 +51,24 @@ selectionTable = dbSelection['android_apps']
 # Connect to db where we store translations
 db = dataset.connect('sqlite:///{}'.format(TRANSLATED_FILE))
 # table with translated fields
-transTable = db['translated_android_apps']
+transTable = db['android_apps']
 
 # The target and source languages
-source = 'es'
-target = 'en'
-country = 'es'
+# TO DO: make these arguments
+try: source = sys.argv[1]
+except: source = 'es'
+
+try: country = sys.argv[2]
+except: country = 'mx'
+
+try: target = sys.argv[3]
+except: target = 'en'
 
 # get selection of apps
-result = dbApps.query('SELECT id, appId, title, summary, description,genreId, developer, LANG, COUNTRY FROM android_apps WHERE LANG=\''+source + '\' and COUNTRY=\''+country+'\' ORDER BY RANDOM() LIMIT 50; ')
+result = dbApps.query('SELECT id, appId, title, summary, description, genreId, developer, developerId, LANG, COUNTRY, permissions FROM android_apps WHERE LANG=\''+source + '\' and COUNTRY=\''+country+'\' limit 2;')
 
 # Other sql options
+# ORDER BY RANDOM() LIMIT 50 # select 50 random
 #and appId IN (\'com.\')  # for selecting among list of apps
 #and id > 8692') # for selecting range of apps, e.g., after a certain point 
 #and appId=\'com.\'' # for selecting specific app
@@ -82,9 +89,11 @@ for row in result:
     idText = row['id']
     appIdText = row['appId']
     developerText = row['developer']
+    developerIdText = row['developerId']
     langText = row['LANG']
     countryText = row['COUNTRY']
     genreIdText = row['genreId']
+    permissionsText = row['permissions']
     
     # translate text - call function
     translatedTitleText = translateFunc(app_dict['title'])
@@ -94,12 +103,12 @@ for row in result:
     translatedDescriptionText = translateFunc(app_dict['descriptionText'])
     #print("translated Title text: " + translatedDescriptionText)
     
-    #insert into new db's translation table
-    transTable.insert(dict(oldId=idText, appId=appIdText, title=translatedTitleText, summary=translatedSummaryText, description=translatedDescriptionText, developer = developerText, LANG=langText, COUNTRY=countryText, genreId=genreIdText))
+    #insert into translated apps db's android_apps table
+    transTable.insert(dict(oldId=idText, appId=appIdText, title=translatedTitleText, summary=translatedSummaryText, description=translatedDescriptionText, developer = developerText,developerId=developerIdText, LANG=langText, COUNTRY=countryText, genreId=genreIdText, permissions=permissionsText))
 
-    #insert original language data into selection db 
-    selectionTable.insert(dict(id=idText, appId=appIdText, title=row['title'], summary=row['summary'], description=row['description'], developer = developerText, LANG=langText, COUNTRY=countryText, genreId=genreIdText))
+    #insert original language data into selection db's android_apps table
+    selectionTable.insert(dict(id=idText, appId=appIdText, title=row['title'], summary=row['summary'], description=row['description'], developer = developerText, developerId=developerIdText, LANG=langText, COUNTRY=countryText, genreId=genreIdText, permissions=permissionsText))
     
-    #sleep for 1 second 
+    #sleep for 2 seconds 
     time.sleep(2)
     # may need to use timeout API method 
